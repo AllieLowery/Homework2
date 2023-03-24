bd085bcd5604b2bcea9440b3d7a06ea38790ea95
Allison Lowery <a.lowery@setonhill.edu>
2023-03-23 23:27:58 -0400

    Initial Commit

---
+++ Homework2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework2
+//
+//  Created by Allie Lowery on 3/23/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
